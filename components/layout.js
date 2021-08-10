723731eeb1e72426dd0c20c7ba198d9fb35b3337
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-10 18:39:12 +0200

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Benjamin DESOLE'
+export const siteTitle = 'Ben Blog'
 
 export default function Layout({ children, home }) {
   return (
