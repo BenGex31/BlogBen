8b34a377ecfb2bf399962d3c91a132f1d2b490b4
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-10 18:48:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import React from "react";
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 export default function Home() {
   return (
@@ -16,13 +18,21 @@ export default function Home() {
           You can contact me on :
           <ul>
             <li>
-              <a href='https://www.linkedin.com/in/benjamindesole/' target="_blank">LinkedIn</a>
+              <a
+                href='https://www.linkedin.com/in/benjamindesole/'
+                target='_blank'>
+                LinkedIn <LinkedInIcon />
+              </a>
             </li>
             <li>
-              <a href='https://twitter.com/BenjaminDesole' target="_blank">Twitter</a>
+              <a href='https://twitter.com/BenjaminDesole' target='_blank'>
+                Twitter
+              </a>
             </li>
             <li>
-              <a href='https://github.com/BenGex31' target="_blank">GitHub</a>
+              <a href='https://github.com/BenGex31' target='_blank'>
+                GitHub
+              </a>
             </li>
           </ul>
         </p>
