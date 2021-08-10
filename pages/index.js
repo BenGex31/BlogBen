bdee3d03bf7187cb056e2d31851a19f1db48bf6c
Benjamin Desole <benjamin.desole@gmail.com>
2021-08-10 18:39:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+/** @format */
+
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -9,12 +11,26 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          I'm a French frontend developer.<br></br>
+          You can contact me on :
+          <ul>
+            <li>
+              <a href='https://www.linkedin.com/in/benjamindesole/' target="_blank" >LinkedIn</a>
+            </li>
+            <li>
+              <a href='https://twitter.com/BenjaminDesole' target="_blank" >Twitter</a>
+            </li>
+            <li>
+              <a href='https://github.com/BenGex31' target="_blank" >GitHub</a>
+            </li>
+          </ul>
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{" "}
+          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
       </section>
     </Layout>
-  )
+  );
 }
